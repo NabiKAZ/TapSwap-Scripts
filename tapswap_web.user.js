2e534ca2446e8abb6019e1a3dd464ad91e972d24
Nabi KaramAliZadeh <NabiKAZ@gmail.com>
2024-06-29 19:45:06 +0330

    Create tapswap_web.user.js

---
+++ tapswap_web.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         TapSwap open on the web
+// @namespace    https://github.com/NabiKAZ/TapSwap-Scripts
+// @version      0.1
+// @description  TapSwap open on the web
+// @author       https://twitter.com/NabiKAZ
+// @match        https://app.tapswap.club/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.ontouchstart = true;
+})();
